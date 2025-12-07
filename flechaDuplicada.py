179ef6a19b93d6c1cde76dd86a3ef7189f4f11d3
danieler9324 <danielviramontes562@gmail.com>
2025-12-07 17:33:55 -0600

    ejemplo de la flecha

---
+++ flechaDuplicada.py
@@ -0,0 +1,22 @@
+#Flecha normal
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+
+
+
+#Flecha doble
+print("    *"*2)
+print("   * *"*2)
+print("  *   *"*2)
+print(" *     *"*2)
+print("***   ***"*2)
+print("  *   *"*2)
+print("  *   *"*2)
+print("  *****"*2)

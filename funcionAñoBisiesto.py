1bc49d1ef54ac55af0f25bee13dcbc5d731b2195
danieler9324 <danielviramontes562@gmail.com>
2025-12-22 14:32:08 -0600

    ejemplo de funcion actualizado

--- funcionAñoBisiesto.py
+++ funcionAñoBisiesto.py
@@ -7,12 +7,26 @@ def is_year_leap(year):
         return True
     return False
 
-test_data = [1900, 2000, 2016, 1987]
-test_results = [False, True, True, False]
-for i in range(len(test_data)):
-    yr = test_data[i]
-    print(yr,"->",end="")
-    result = is_year_leap(yr)
+def days_in_month(year, month):
+
+    if month < 1 or month > 12:
+        return None
+
+    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    if month == 2 and is_year_leap(year):
+        return 29
+
+    return days[month - 1]
+
+test_years = [1900, 2000, 2016, 1987]
+test_months = [2, 2, 1, 11]
+test_results = [28, 29, 31, 30]
+for i in range(len(test_years)):
+    yr = test_years[i]
+    mo = test_months[i]
+    print(yr, mo, "->", end="")
+    result = days_in_month(yr, mo)
     if result == test_results[i]:
         print("OK")
     else:

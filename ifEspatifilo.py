fef7c87d1d82e40060a486b949222122b6389308
Danieler9324 <danielviramontes562@gmail.com>
2025-12-25 13:56:15 -0600

    ejemplo de if agregado

---
+++ ifEspatifilo.py
@@ -0,0 +1,8 @@
+palabra = input("Ingresa una palabra: ")
+
+if palabra == "ESPATIFILO":
+    print("Si, ¡El Espatifilo! es la mejor planta de todos los tiempos!")
+elif palabra == "espatifilo":
+    print("No, ¡quiero un gran Espatifilo!")
+else:
+    print("¡Espatifilo!, no ", palabra)
